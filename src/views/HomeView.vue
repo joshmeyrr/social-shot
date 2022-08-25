cbebb233f8d794a5e66dfa12340139e73c7d8373
Josh <meyer.josh8@gmail.com>
2022-08-24 21:26:15 -0500

    build: initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<template>
+  <main class="flex flex-col gap-2 justify-center">
+    <div
+      v-for="(card, index) in cards"
+      :key="index"
+      class="bg-gray-700 w-72 p-8 rounded"
+    >
+      <h2 class="text-4xl uppercase font-black">
+        {{ card.title }}
+        <span class="text-orange-400 font-black">#{{ index }}</span>
+      </h2>
+      <p>
+        {{ card.description }}
+      </p>
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const cards = ref([
+  {
+    title: "Card",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, eveniet cumque. Consequatur natus dolorum laboriosam at saepe reprehenderit molestiae, officiis animi, quibusdam fuga corporis laborum!",
+  },
+  {
+    title: "Employee",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, eveniet cumque. Consequatur natus dolorum laboriosam at saepe reprehenderit molestiae, officiis animi, quibusdam fuga corporis laborum!",
+  },
+  {
+    title: "Customer",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, eveniet cumque. Consequatur natus dolorum laboriosam at saepe reprehenderit molestiae, officiis animi, quibusdam fuga corporis laborum!",
+  },
+]);
+</script>
